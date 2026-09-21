0e2fedb0f36fc927b9cebfd334fa7d64a3b59c32
Ravi Joshi <joshiravi2706@gmail.com>
2026-09-21 19:34:43 +0530

    Deleted cd form build.sh

--- LoaftBackend/LoaftApi/build.sh
+++ LoaftBackend/LoaftApi/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-# Step into the folder where manage.py actually lives
-cd LoaftBackend/Loft/
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
