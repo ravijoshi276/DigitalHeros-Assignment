62a0fb8a50bb9e565cd599830e703b64050210fe
Ravi Joshi <joshiravi2706@gmail.com>
2026-09-21 19:31:50 +0530

    Modified build.sh

--- LoaftBackend/LoaftApi/build.sh
+++ LoaftBackend/LoaftApi/build.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
+set -o errexit
+
 echo "Installing dependencies..."
 pip install -r requirements.txt
+
+# Step into the folder where manage.py actually lives
+cd LoaftBackend/Loft/
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
