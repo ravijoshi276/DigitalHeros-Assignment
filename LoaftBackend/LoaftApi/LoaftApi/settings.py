d1928fcd399f6690d7798e568da6b94151ee3adb
Ravi Joshi <joshiravi2706@gmail.com>
2026-09-21 21:32:30 +0530

    Modified debug env

--- LoaftBackend/LoaftApi/LoaftApi/settings.py
+++ LoaftBackend/LoaftApi/LoaftApi/settings.py
@@ -28,7 +28,8 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 
 #  Host Management
