620454a227ef83a9662bebc224c626adb5d5e84e
Ravi Joshi <joshiravi2706@gmail.com>
2026-09-21 18:02:16 +0530

    Modified frontend main.jsx AuthContext

--- LoftFrontend/Loft/src/main.jsx
+++ LoftFrontend/Loft/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './context/Authcontext'
+import { AuthProvider } from './context/AuthContext'
 import './index.css'
 import App from './App.jsx'
 
